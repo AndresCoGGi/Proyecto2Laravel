8d65a8fa44288bafaa6cbb0bd0b752e84edc6483
AndresCoGGi <andres199410@hotmail.com>
2018-03-06 21:46:53 -0500

    segundo commit

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,27 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/usuarios',function(){
+   return 'usuarios';
+});
+
+//usuarios/nuevo no coincide coincide con id
+Route::get('usuarios/{id}',function($id){
+    return "mostrando detalle del usuario: $id";
+})->where('id','[0-9]+');//para validar que la ruta sea con un numero
+
+Route::get('usuarios/nuevo',function(){
+      return 'crear nuevo usuario';
+
+});
+//laravel toma la primera ruta que el encuentre /usuarios/'?????'
+
+Route::get('/saludo/{name}/{nickname}',function($name,$nickname=null){
+   if($nickname){
+       return "bienvenido {$name}, tu apodo es {$nickname}";
+   }else{
+       return "bienvenido {$name}, no tienes apodo";
+   }
+   
+});
